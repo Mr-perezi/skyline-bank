318789b1745c338156c1d6968553782498d3ccdd
Mr-perezi <marvinswallace6@gmail.com>
2026-03-23 22:55:29 +0000

    save

--- bank_site/settings.py
+++ bank_site/settings.py
@@ -33,19 +33,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Use environment variable for database URL
 DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL:
     # Parse the URL if it's set (for production)
     DATABASES = {
         'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
     }
-else:
-    # Local development (SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
 cloudinary.config(
     cloud_name="dlzn0moho",
     api_key="563396395915366",
